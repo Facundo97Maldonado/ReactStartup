d670d5598a9e55d000d950583130b10b5868ac67
Facundo Maldonado <facumaldonado319@gmail.com>
2017-05-18 15:44:57 -0300

    Commited just for review

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-import TestComponent from './testComponent'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <TestComponent />
-  }
+var SpotifyApi = require('spotify-web-api-node');
+
+var spotifyWebApi = new SpotifyApi({
+	clientId: 'fcecfc72172e4cd267473117a1cbd4d',
+	clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+	redirectUri: 'http://www.example.com/callback'
+});
+
+class ArtistComponent extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			artistList: [],
+			showArtist: false,
+
+		};
+		this.searchArtist = this.searchArtist.bind(this);
+		this.handleAlbumsShow = this.handleAlbumsShow.bind(this);
+	}
+
+
+	searchArtist(){
+		spotifyWebApi.searchArtists(this.props.name, {limit: 10, offset: 0})
+		.then((data) => {
+			this.state.artistList.push(data.body.artists.items)
+
+			this.setState({
+				artistList: this.state.artistList[0],
+				showArtist: false 
+			});
+		},(error) => {
+			console.error(error);
+		})
+	}
+
+	handleAlbumsShow(){
+		this.setState({
+			showAlbum: true
+		})
+	}
+
+	render () {
+	  return (
+	   	<div>
+	   		<h1>Search an Artist and display his Albums</h1>
+	   	</div>
+	  )
+	}
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<ArtistComponent />, document.getElementById('app'));
+
